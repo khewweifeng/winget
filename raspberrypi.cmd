deb8a71af1eace69cca3e2cbfd6f9858b3e7ca21
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2025-02-20 17:04:38 +0800

    Revert "Update raspberrypi.cmd"
    This reverts commit 1103417b9beb5c2491be01f7904069aa7e1d5dd5.

--- raspberrypi.cmd
+++ raspberrypi.cmd
@@ -1,16 +1,4 @@
 @echo off
-setlocal
+wt ssh pi@raspberrypi.local
+@echo raspberrypi.local| clip
 
-rem Set the directory path
-set "dirPath=C:\Users\kheww\OneDrive\Pictures\Screenshots"
-
-rem Check if the directory exists
-if exist "%dirPath%" (
-    rem Delete all files in the directory
-    del /Q "%dirPath%\*"
-    
-    rem Open the folder
-    start "" "%dirPath%"
-)
-
-endlocal
